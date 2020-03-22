fb0d3251ecc26be53875acec270bb79987e46ee0
br1code <brunogiovagnoli@gmail.com>
2020-03-21 22:46:59 -0300

    fix white space + delete debug.log file

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const MAX_RIOT_POINTS = 5000;
 var blueEssence = document.getElementById('blue_essence');
 var riotPoints = document.getElementById('riot_points');
 
-blueEssence.innerText  = getRandomNumber(MAX_BLUE_ESSENCE);
-riotPoints.innerText  = getRandomNumber(MAX_RIOT_POINTS);
+blueEssence.innerText = getRandomNumber(MAX_BLUE_ESSENCE);
+riotPoints.innerText = getRandomNumber(MAX_RIOT_POINTS);
 
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max) + 1;
